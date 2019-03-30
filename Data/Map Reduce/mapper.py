dd2f57f4e0f3fb1d45bd7e0fa74c58218ec7a58a
a_m_a_n <amanbhayana@yahoo.in>
2019-03-30 14:57:40 -0400

    Refactored code

--- Data/Map Reduce/mapper.py
+++ Data/Map Reduce/mapper.py
@@ -7,51 +7,55 @@ import string
 from nltk.stem import PorterStemmer
 
 
-ps = PorterStemmer()
-import nltk
-# if there is a problem in downloading nltk data, then run the Install Certificates.command available in your Python folder and then execute the download command
-nltk.download('stopwords')
-f = open("/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/cleanedTwitterData.txt", "w+")
 
-
-deleteString = [
-    r"""
-        (?:
-            [:=;] # Eyes
-            [oO\-]? # Nose (optional)
-            [D\)\]\(\]/\\OpP] # Mouth
-        )"""
-    ,
-    r'(?<=\D)[.,]|[.,](?=\D)',
-    r'<[^>]+>',  # HTML tags
-    r'(?:@[\w_]+)',  # @-mentions
-    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
-    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
-    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
-    r'[^\x00-\x7f]'
-]
-
-
-deleteWordsRegEx = re.compile(r'(' + ',' +'.' + '|'.join(deleteString) + ')', re.VERBOSE | re.IGNORECASE)
-
-
-def tokenize(text):
+def tokenize(text, regExForDeletingWords):
+    ps = PorterStemmer()
     resultTokens = []
     tokens = [t for t in text.split()]
     for token in tokens:
-        match = re.search(deleteWordsRegEx,token)
+        match = re.search(regExForDeletingWords,token)
         type(match)
         if match is None:
             ps.stem(token)
             resultTokens.append(token)
     return resultTokens
 
-data = pd.read_csv("/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/twitterdata.csv")
-punctuationToRemove = list(string.punctuation)
-stop = stopwords.words('english') + punctuationToRemove
-for tweet in data.text:
-    line =' '.join([term for term in tokenize(tweet) if term not in stop])
 
-    f.write(line)
+def SetupDataClean():
+    import nltk
+    # if there is a problem in downloading nltk data, then run the Install Certificates.command available in your Python folder and then execute the download command
+    nltk.download('stopwords')
+    deleteString = [
+        r"""
+            (?:
+                [:=;] # Eyes
+                [oO\-]? # Nose (optional)
+                [D\)\]\(\]/\\OpP] # Mouth
+            )"""
+        ,
+        r'(?<=\D)[.,]|[.,](?=\D)',
+        r'<[^>]+>',  # HTML tags
+        r'(?:@[\w_]+)',  # @-mentions
+        r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+        r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+        r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+        r'[^\x00-\x7f]'
+    ]
+    deleteWordsRegEx = re.compile(r'(' + ',' +'.' + '|'.join(deleteString) + ')', re.VERBOSE | re.IGNORECASE)
+    return deleteWordsRegEx
 
-f.close()
+
+def CleanData(inputFile, outputFile, regEx):
+    output = open(outputFile, "w+")
+    data = pd.read_csv(inputFile)
+    punctuationToRemove = list(string.punctuation)
+    stop = stopwords.words('english') + punctuationToRemove
+    for tweet in data.text:
+        line =' '.join([term for term in tokenize(tweet, regEx) if term not in stop])
+        output.write(line)
+    output.close()
+    print('Cleaned data saved at ' , outputFile)
+
+if __name__ == '__main__':
+    regEx = SetupDataClean()
+    CleanData(inputFile="/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/twitterdata.csv",outputFile="/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/cleanedTwitterData.txt", regEx=regEx)
