372917e7453a5b96251982faebc481b35ac534e3
a_m_a_n <amanbhayana@yahoo.in>
2019-03-31 20:28:54 -0400

    Added reduceer

--- Data/Map Reduce/reducer.py
+++ Data/Map Reduce/reducer.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python
 import sys
-word = None
-prev_word = None
-prev_count = 0;
+
+wordCountDict = {}
 for line in sys.stdin:
     line = line.strip()
-    lt =  line.split('\t')
-    if (len(lt)!=2):
-        continue;
-    word,count = lt[0],lt[1]
-    count = int(count)
-    if (prev_word == None):
-        prev_word = word
-        continue
-    elif (prev_word == word):
-        prev_count += count
+    lineTokens = line.split('\t')
+    word,count = lineTokens[0],lineTokens[1]
+    if word in wordCountDict:
+        wordCountDict[word] += 1
     else:
-        print(prev_word+"\t"+str(prev_count))
-        prev_word = word
-        prev_count = count
-print (prev_word+"\t"+str(prev_count))
+        wordCountDict[word] = int(count)
+for word in wordCountDict.keys():
+    print(word + "\t" +  wordCountDict[word])
