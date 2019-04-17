7d82aca2f4119b5eb31a57d2d24841ba9e111b8b
a_m_a_n <amanbhayana@yahoo.in>
2019-04-17 17:16:03 -0400

    Fixed the Ny times data collection code. Now each para will be saved in another file.

--- Data/NyTimes/WebScrap.py
+++ Data/NyTimes/WebScrap.py
@@ -2,22 +2,28 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def ScrapData(inputFileName, outputFileName):
-
-    textFile = open(outputFileName,"wb")
+def ScrapData(inputFileName):
+    i=1
+    text =""
+    #textFile = open(outputFileName,"wb")
     with open(inputFileName, "r") as f:
+        suffix =inputFileName.split(".")[0]
         try:
 
             for htmlLink in f:
                 print(htmlLink)
                 page =requests.get(htmlLink.split("  ")[1])
-
                 data = page.text
                 soup = BeautifulSoup(data, 'html.parser')
                 for article in soup.find_all('article'):
                     for para in article.find_all('p'):
-                        textFile.write(para.get_text().encode('utf-8'))
+                        text += para.get_text().encode('utf-8')
+                if text != "":
+                    textFile = open("/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/NyTimes/Scrapped Data/"+str(i) + "_" + suffix +".txt","wb")
+                    textFile.write(text)
+                    i= i+1
+                    text =""
         except:
             textFile.close()
 if __name__ == '__main__':
-    ScrapData(inputFileName="nhl.txt", outputFileName="NhlPlainText.txt")
+    ScrapData(inputFileName="football.txt")
