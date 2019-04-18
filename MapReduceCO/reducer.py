9ead6d4a2f8ca119f1d4f15629b85e93fb368267
a_m_a_n <amanbhayana@yahoo.in>
2019-04-18 03:29:07 -0400

    added reducer file

--- MapReduceCO/reducer.py
+++ MapReduceCO/reducer.py
@@ -1,46 +1,39 @@
 #!/usr/bin/env python
-"""reducer.py"""
 
 from operator import itemgetter
-
 import sys
+import json
 
-curr_word = ""
-curr_count = 0
-word = curr_word
-wordpair = curr_word
+current_word = None
+current_dict = {}
+word = None
+
 
 for line in sys.stdin:
-    line = line.strip()
-<<<<<<< HEAD
-<<<<<<< HEAD
-    lt =  line.split('\t')
-    if len(lt)!=2:
-        continue;
-    word,count = lt[0],lt[1]
-    count = int(count)
-    if prev_word == None:
-        prev_word = word
-=======
-=======
->>>>>>> 5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
-    wordpair, count = line.split('\t', 1)
-    try:
-        count = int(count)
-    except ValueError:
-<<<<<<< HEAD
->>>>>>> 5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
-=======
->>>>>>> 5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
-        continue
-    if curr_word == wordpair:
-        curr_count += count
-    else:
-        if curr_word:
-            # write result to STDOUT
-            print('%s\t%s' % (curr_word, curr_count))
-        curr_count = count
-        curr_word = wordpair
 
-if curr_word == wordpair:
-    print('%s\t%s' % (curr_word, curr_count))
+    line = line.strip()
+
+
+    word, stripe = line.split('\t', 1)
+
+
+    try:
+        stripe = json.loads(stripe)
+    except:
+        print "error"
+        continue
+
+    if current_word == word:
+        for entries in stripe:
+            if entries not in current_dict:
+                current_dict[entries] = 0
+            current_dict[entries] += int(stripe[entries])
+    else:
+        if current_word:
+
+            print '%s\t%s' % (current_word, json.dumps(current_dict))
+        current_dict = stripe
+        current_word = word
+
+if current_word == word:
+    print '%s\t%s' % (current_word, json.dumps(current_dict))
