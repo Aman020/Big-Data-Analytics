78cd53befc367ea8ee95c3a7ed5412b0208edf23
a_m_a_n <amanbhayana@yahoo.in>
2019-04-18 12:38:54 -0400

    added the correct code of mapper and reducer

--- MapReduceCO/reducer.py
+++ MapReduceCO/reducer.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python
+"""reducer.py"""
 
 from operator import itemgetter
+
 import sys
-import json
 
-current_word = None
-current_dict = {}
-word = None
-
+curr_word = ""
+curr_count = 0
+word = curr_word
+wordpair = curr_word
 
 for line in sys.stdin:
-
     line = line.strip()
-
-
-    word, stripe = line.split('\t', 1)
-
-
+    wordpair, count = line.split('\t', 1)
     try:
-        stripe = json.loads(stripe)
-    except:
-        print "error"
+        count = int(count)
+    except ValueError:
         continue
-
-    if current_word == word:
-        for entries in stripe:
-            if entries not in current_dict:
-                current_dict[entries] = 0
-            current_dict[entries] += int(stripe[entries])
+    if curr_word == wordpair:
+        curr_count += count
     else:
-        if current_word:
+        if curr_word:
+            # write result to STDOUT
+            print('%s\t%s' % (curr_word, curr_count))
+        curr_count = count
+        curr_word = wordpair
 
-            print '%s\t%s' % (current_word, json.dumps(current_dict))
-        current_dict = stripe
-        current_word = word
-
-if current_word == word:
-    print '%s\t%s' % (current_word, json.dumps(current_dict))
+if curr_word == wordpair:
+    print('%s\t%s' % (curr_word, curr_count))
