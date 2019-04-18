92743686e29c9cb99ec2f4b37cf8b8cb95b36079
a_m_a_n <amanbhayana@yahoo.in>
2019-04-18 19:37:18 -0400

    changed mapper for CO to find top 10 words

--- MapReduceCO/reducer.py
+++ MapReduceCO/reducer.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
 """reducer.py"""
+
+from operator import itemgetter
 import sys
+import collections
 
-currentWord = None
-currentCount = 0
-word = currentWord
-wordCoOccur = currentWord
+current_word = None
+current_count = 0
+word = None
 
+wordDict = {}
+
+# input comes from STDIN
 for line in sys.stdin:
+    # remove leading and trailing whitespace
     line = line.strip()
-    line = line.lower()
-    wordCoOccur, count = line.split('\t', 1)
-    count = int(count)
-    if currentWord == wordCoOccur:
-        currentCount = currentCount + count
-    else:
-        if currentWord:
-            print('%s\t%s' % (currentWord, currentCount))
-            currentCount = count
-        currentWord = wordCoOccur
-if currentWord == wordCoOccur:
-    print('%s\t%s' % (currentWord, currentCount))
 
+    # parse the input we got from mapper.py
+    word, count = line.split('\t', 1)
 
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+
+    try :
+        wordDict[word] = wordDict[word] + count
+    except:
+        wordDict[word] = count
+
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+
+wordDict = sorted(wordDict.items(), key= lambda kv:(kv[1],kv[0]), reverse = True)
+
+for word in wordDict:
+    print('%s\t%s' % (word[0], word[1]))
