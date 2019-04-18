604159fc65ade90faacecd2a6eda19f002437c51
a_m_a_n <amanbhayana@yahoo.in>
2019-04-18 03:27:32 -0400

    added mapper file

--- MapReduceCO/mapper.py
+++ MapReduceCO/mapper.py
@@ -2,18 +2,13 @@
 """mapper.py"""
 
 import sys
-<<<<<<< HEAD
-<<<<<<< HEAD
-myWords = ["football","real","team","madrid","game","match","win","one","first","play","los", "con", "que", "del","win","messi"]
-=======
-topwords = ["football","real","team","madrid","game","match","win","one","first","play","los", "con", "que", "del"]
-tempWord = ""
->>>>>>> 5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
+myWords = ["football","real","team","madrid","game","match","win","one","first","play"]
+wordsDict = {}
 for line in sys.stdin:
-    # remove leading and trailing whitespace
+    if len(line) == 0:
+        continue;
     line = line.strip()
-<<<<<<< HEAD
-    line = line.lower();
+    line = line.lower()
     wordsDict = {}
     for word in line:
         if word in myWords:
@@ -25,21 +20,3 @@ for line in sys.stdin:
             if myWords[i2] not in wordsDict:
                 continue;
             print (myWords[loc]+","+myWords[i2]+"\t1")
-=======
-=======
-topwords = ["football","real","team","madrid","game","match","win","one","first","play","los", "con", "que", "del"]
-tempWord = ""
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
->>>>>>> 5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
-    # split the line into words
-    words = line.split()
-    for topword in topwords:
-        for i in range(0, len(words)-1):
-            if words[i] == topword:
-                print("%s|%s\t%s" % (words[i],words[i+1], 1))
-<<<<<<< HEAD
->>>>>>> 5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
-=======
->>>>>>> 5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
