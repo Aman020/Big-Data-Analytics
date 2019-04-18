5e544b47aa76ed2eb32fab0ec27402e6d53d38eb
Pratik Agarwal <pagarwal@buffalo.edu>
2019-04-18 02:50:21 -0400

    Added Twitter cleaned data, removed unwanted files and MR code for Word CO

--- MapReduceCO/mapper.py
+++ MapReduceCO/mapper.py
@@ -2,21 +2,14 @@
 """mapper.py"""
 
 import sys
-myWords = []
-wordsDict = {}
+topwords = ["football","real","team","madrid","game","match","win","one","first","play","los", "con", "que", "del"]
+tempWord = ""
 for line in sys.stdin:
-    if len(line) == 0:
-        continue;
+    # remove leading and trailing whitespace
     line = line.strip()
-    line = line.lower();
-    wordsDict = {}
-    for word in line:
-        if word in myWords:
-            wordsDict[word] = True
-    for loc,word in enumerate(myWords):
-        if word not in wordsDict:
-            continue;
-        for i2 in range(loc+1,len(myWords)):
-            if myWords[i2] not in wordsDict:
-                continue;
-            print (myWords[loc]+","+myWords[i2]+"\t1")
+    # split the line into words
+    words = line.split()
+    for topword in topwords:
+        for i in range(0, len(words)-1):
+            if words[i] == topword:
+                print("%s|%s\t%s" % (words[i],words[i+1], 1))
--- MapReduceCO/reducer.py
+++ MapReduceCO/reducer.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 """reducer.py"""
 
+from operator import itemgetter
+
 import sys
-word = None
-prev_word = None
-prev_count = 0;
+
+curr_word = ""
+curr_count = 0
+word = curr_word
+wordpair = curr_word
+
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-    lt =  line.split('\t')
-    if len(lt)!=2:
-        continue;
-    word,count = lt[0],lt[1]
-    count = int(count)
-    if prev_word is None:
-        prev_word = word
+    wordpair, count = line.split('\t', 1)
+    try:
+        count = int(count)
+    except ValueError:
         continue
-    elif prev_word == word:
-        prev_count += count
+    if curr_word == wordpair:
+        curr_count += count
     else:
-        print(prev_word+"\t"+str(prev_count))
-        prev_word = word
-        prev_count = count
-print (prev_word+"\t"+str(prev_count))
+        if curr_word:
+            # write result to STDOUT
+            print('%s\t%s' % (curr_word, curr_count))
+        curr_count = count
+        curr_word = wordpair
+
+if curr_word == wordpair:
+    print('%s\t%s' % (curr_word, curr_count))
