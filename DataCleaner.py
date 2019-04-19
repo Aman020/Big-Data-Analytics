70007385238458fb127f4dffbdc93c28a99592e8
Pratik Agarwal <pagarwal@buffalo.edu>
2019-04-19 12:49:31 -0400

    Structured the project

--- DataCleaner.py
+++ DataCleaner.py
@@ -34,9 +34,10 @@ def SetupDataClean():
 
 
 
-def Clean(regExForDeletingWords,inputFileName, outputFileName):
+def Map(regExForDeletingWords,inputFileName, outputFileName):
 
     textFile = open(outputFileName,"w")
+    #with open(inputFileName, "r", encoding='utf-8') as file:
     for filelineno, line in enumerate(open(inputFileName)):
             if len(line) == 0:
                 continue;
@@ -45,20 +46,25 @@ def Clean(regExForDeletingWords,inputFileName, outputFileName):
             punctuationToRemove = list(string.punctuation)
             stop = stopwords.words('english') + punctuationToRemove
             new_line = [term for term in tokenize(line,regExForDeletingWords) if not term in stop]
+            new_line = [word for word in new_line if not len(word) == 1]
+            new_line = [word for word in new_line if not len(word) == 2]
             for word in new_line:
-                textFile.write(word + " ")
+                textFile.write(word.lower() + " ")
     textFile.close();
 
 
 if __name__ == '__main__':
 
     regEx = SetupDataClean()
-    for filename in os.listdir("/Drive/UB/Spring19/DIC/Project2/Lab2/Data/Common Crawl/"):
-            if filename.endswith(".txt"):
-                print(str(filename))
-                Clean(regEx, "/Drive/UB/Spring19/DIC/Project2/Lab2/Data/Common Crawl/" +filename,"/Drive/UB/Spring19/DIC/Project2/Lab2/Data/Common Crawl/CleanedData/" + str(filename).split(".")[0] + "_cleaned.txt")
+    Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/mlbText.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/cleanedMlb.txt")
+    # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/Aman_data/football.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/Cleanedfootball.txt")
+    # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/Aman_data/fifa.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/Cleanedfifa.txt")
+    # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/Aman_data/messi.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/Cleanedmessi.txt")
+    # # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/messi.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/messiCleaned.txt")
+    # # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/mlsText.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/mlsTextCleaned.txt")
+    # # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/nbaText.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/nbaTextCleaned.txt")
+    # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/nhlText.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/nhlTextCleaned.txt")
+    # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/realmadrid.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/realmadridCleaned.txt")
+    # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/ronaldo.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/ronaldoCleaned.txt")
+    # Map(regEx,"/Users/dev/PycharmProjects/DIC/Twitter/zidane.txt", "/Users/dev/PycharmProjects/DIC/Cleaned/Twitter/zidaneCleaned.txt")
 
-    #Map(regEx,"/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/footballText.txt", "/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/CleanedData/CleanedFootballData.txt")
-    #Clean(regEx,"/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/nbaText.txt", "/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/CleanedData/CleanedNbaData.txt")
-    #Clean(regEx,"/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/nhlText.txt", "/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/CleanedData/CleanedNhlData.txt")
-    #Clean(regEx, "/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/mlbText.txt","/Users/aman/PycharmProjects/DIC/BigDataAnalysis/Data/Twitter/CleanedData/CleanedMlbData.txt")
